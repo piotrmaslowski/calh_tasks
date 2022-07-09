947fed0a76b44d808c6a2db1bf8e69b7b0bd8dcc
Piotrek <piotr.maslowski@ing.pl>
2022-07-10 01:32:39 +0200

    task1 completed

--- task1/task1.py
+++ task1/task1.py
@@ -3,8 +3,13 @@ import xml.etree.ElementTree as ET
 
 
 def task1_solution():
-    pass
-
+    r = requests.get('https://coding-academy.pl/all_customers')
+    if r.status_code == 200:
+        root = ET.fromstring(r.text)
+        with open("task1_solution.txt", "wt") as file:
+            for child in root:
+                if child.tag == "customer":
+                    file.write(child.text + "\n")
 
 if __name__ == '__main__':
     task1_solution()
